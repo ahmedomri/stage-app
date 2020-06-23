f23a5fcae5ad52a136f8820ffd8debd99cdc3995
ytoullichi <ytoullichi@gmail.com>
2020-06-23 04:25:41 +0100

    Update login.component.ts

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {NgForm, Validators ,FormGroup,FormControl, FormBuilder} from '@angular/forms';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Router } from "@angular/router";
 
@@ -15,15 +15,23 @@ export class LoginComponent implements OnInit {
   username: string;
   password : string;
   status=[];
+  ngform: FormGroup;
+  erreurMessage: String;
   @Input() message:string;
 
-  constructor(private _httpClient: HttpClient ,private router: Router) { }
+  constructor(private _httpClient: HttpClient ,private router: Router, private formBuilder: FormBuilder) { }
 
 
   ngOnInit() {
-    
+    this.ngform = this.formBuilder.group({
+      username: [null, [Validators.required]],
+      password: [null, Validators.required],
+    });
+  
   }
-
+  
+  
+  
   onSubmit(form: NgForm) {
     let params = new HttpParams().set("username",this.username).set("password",this.password); //Create new HttpParams
     
